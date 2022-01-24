528122e9f3267ffea2e32f133c528a14f863d6f0
RLowe <richard950pt@gmail.com>
2022-01-24 15:06:14 -0500

    commit

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Navbar from './Navbar';
 import {
   BrowserRouter as Router,
   Switch,
@@ -21,42 +21,7 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <nav class="navbar navbar-dark bg-dark navbar-custom">
-          <div class="container">
-            <a class="navbar-brand" href="/">TACTIC</a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent1"
-              aria-controls="navbarSupportedContent1"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div
-              class="collapse navbar-collapse"
-              id="navbarSupportedContent1"
-            >
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                  <Link to="/jobs">All Jobs</Link> <br />
-                </li>
-                <li class="nav-item">
-                  <Link to="/jobassets">Job Assets</Link>
-                </li>
-                <li class="nav-item">
-                  <Link to="/assets">Assets</Link>
-                </li>
-                <li class="nav-item">
-                  <Link to="/test">Test</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navbar />
         <Switch>
           <Route exact path="/jobs" component={Jobs} />
           <Route exact path="/test" component={Test} />
@@ -71,6 +36,8 @@ class App extends React.Component {
 }
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+  <App />
+</React.StrictMode>,
   document.getElementById('root')
 );
